88909b8e582511f196bb93fdcb90585ef9f47614
iam-kirthana <chippakirthi@gmail.com>
2026-06-05 10:40:52 +0530

    first

---
+++ expense/provider.tf
@@ -0,0 +1,13 @@
+terraform{
+    required_providers{
+        aws =
+         {
+            source = "hashicorp/aws"
+            version = "5.66.0"
+        
+        }
+    }
+}
+providers "aws" {
+    region = "us-east-1"    S
+}
